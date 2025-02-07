4ddbbcf7102fc8a9da6c8e89e61088091c5de69f
Jackson Kaplan <JwyattK@gmail.com>
2025-02-07 14:48:18 -0800

    add stringReplace functionality

---
+++ src/lib/stringReplace.ts
@@ -0,0 +1,23 @@
+type StringDict = { [term: string]: string }
+type StringArray = [string]
+type Terms = StringDict | StringArray
+
+function isArray(terms: Terms): terms is StringArray {
+  return Array.isArray(terms)
+}
+
+
+export const stringReplace = (input: string, terms: Terms): string => {
+
+  const termsAreArray = isArray(terms)
+
+  for (const key in terms) {
+    const replacement = termsAreArray ? "[REDACTED]" : terms[key];
+
+    input = input.replaceAll(key, replacement)
+
+  }
+
+  return input
+}
+
