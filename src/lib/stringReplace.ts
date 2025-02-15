ce8ff03d2afdbb65b4b29053c985b93bd438257b
Jackson Kaplan <JwyattK@gmail.com>
2025-02-15 14:34:29 -0800

    almost done, still need caps-matching

--- src/lib/stringReplace.ts
+++ src/lib/stringReplace.ts
@@ -1,8 +1,8 @@
 import { Trie } from "$lib/trie"
 
-type StringDict = { [term: string]: string }
+type StringMap = Map<string, string>
 type StringArray = string[]
-export type Terms = StringDict | StringArray
+export type Terms = StringMap | StringArray
 
 function isArray(terms: Terms): terms is StringArray {
   return Array.isArray(terms)
@@ -15,13 +15,50 @@ export const stringReplace = (input: string, terms: Terms): string => {
   const termsTrie = new Trie;
   let maxKeyLength = 0
 
-  const termsAreArray = isArray(terms)
-  const termKeys = termsAreArray ? terms : Object.keys(terms);
+  let ret = ''
+
 
   /* TODO: optimize this by only determining max length when terms change */
-  for (const key in termKeys) {
+  for (const key in terms) {
     maxKeyLength = Math.max(key.length, maxKeyLength)
     termsTrie.insert(key)
+  }
+
+
+  let begin = 0;
+  while (begin < input.length) {
+    const next_space = input.indexOf(' ', begin);
+    if (next_space == begin) {
+      ret += ' '
+      ++begin;
+      continue;
+    } else if (next_space == -1) {
+      next_space == input.length
+    }
+
+    const word = input.slice(begin, next_space);
+    const word_lower = word.toLowerCase();
+    if (!/^[A-Za-z0-9]+$/.test(word)) {
+      throw new Error("Non-alphanumeric characters aside from space not yet supported. Offending word: " + word)
+    }
+
+
+    if (termsTrie.contains(word_lower)) {
+
+      let replacement: string;
+
+      if (isArray(terms)) {
+
+      } else {
+      }
+
+
+    } else {
+      ret = ret + word
+    }
+
+
+    begin = next_space
   }
 
 
