c1ba40d9e87896c83ada63e3367c4205f6ddb74a
Jackson Kaplan <JwyattK@gmail.com>
2025-03-17 14:57:15 -0700

    add text blurb

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -9,6 +9,15 @@
 
 <div class="flex items-center justify-center flex-col w-95/100 h-95/100 border-4 border-black">
 	<h1 class="text-3xl grow-0 shrink-0 flex basis-36 items-center">&#x2728 compliance &#x2728</h1>
+	<p class="grow-1 basis-0 w-3/4 text-center">
+		is a text-replacement tool that scans your input text and replaces it using a pre-defined map of
+		target and replacement words. The default replacement map is the <a
+			class="inline underline"
+			href="https://docs.google.com/document/d/13XyaXb4UVyWnsc8-HyBd8Byx-zyd5Nf4x5TTacQUfv0/edit?tab=t.0"
+		>
+			recently unveiled list of words banned by the NSF
+		</a>.
+	</p>
 	<!-- <div class="grow-1 basis-0 my-auto flex justify-center items-center">
 		<button
 			class="h-12 md:w-72 w-48 border-black border-4 rounded-default hover:bg-gray-50"
