ebc24759575231e2da3f74fe745eb84fdb47e3cb
Jackson Kaplan <JwyattK@gmail.com>
2025-03-17 15:05:46 -0700

    text fixup

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -11,7 +11,7 @@
 	<h1 class="text-3xl grow-0 shrink-0 flex basis-36 items-center">&#x2728 compliance &#x2728</h1>
 	<p class="grow-1 basis-0 w-3/4 text-center">
 		is a text-replacement tool that scans your input text and replaces it using a pre-defined map of
-		target and replacement words. The default replacement map is the <a
+		target and replacement words. The default replacement map is based on the <a
 			class="inline underline"
 			href="https://docs.google.com/document/d/13XyaXb4UVyWnsc8-HyBd8Byx-zyd5Nf4x5TTacQUfv0/edit?tab=t.0"
 		>
