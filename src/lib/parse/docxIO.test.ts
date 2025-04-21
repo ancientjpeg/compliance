9b69ab8929b313ea1bf31e9e265f02dec8cc92e3
Jackson Kaplan <JwyattK@gmail.com>
2025-04-21 11:16:46 -0700

    about to refactor xml parser

--- src/lib/parse/docxIO.test.ts
+++ src/lib/parse/docxIO.test.ts
@@ -66,24 +66,6 @@ beforeEach(async () => {
 	// };
 });
 
-test('XML test', async () => {
-	const opts = {
-		ignoreAttributes: false,
-		attributeNamePrefix: '@_',
-		trimValues: false
-		// preserveOrder: true
-	};
-	const xmlString =
-		'<?xml version="1.0" encoding="UTF-8"?><w:document xmlns:r="http://schemas.openxmlformats.org/officeDocument/2006/relationships" xmlns:wp="http://schemas.openxmlformats.org/drawingml/2006/wordprocessingDrawing" xmlns:w="http://schemas.openxmlformats.org/wordprocessingml/2006/main" xmlns:w10="urn:schemas-microsoft-com:office:word" xmlns:w14="http://schemas.microsoft.com/office/word/2010/wordml" xmlns:wps="http://schemas.microsoft.com/office/word/2010/wordprocessingShape" xmlns:wpg="http://schemas.microsoft.com/office/word/2010/wordprocessingGroup" xmlns:mc="http://schemas.openxmlformats.org/markup-compatibility/2006" xmlns:v="urn:schemas-microsoft-com:vml" xmlns:o="urn:schemas-microsoft-com:office:office" xmlns:m="http://schemas.openxmlformats.org/officeDocument/2006/math" mc:Ignorable="w14"><w:body><w:p><w:pPr><w:pStyle w:val="Body A"></w:pStyle></w:pPr><w:r><w:rPr><w:rtl w:val="0"></w:rtl><w:lang w:val="en-US"></w:lang></w:rPr><w:t>diversity Equity and EQUITY</w:t></w:r></w:p><w:p><w:pPr><w:pStyle w:val="Body A"></w:pStyle></w:pPr><w:r><w:rPr><w:rtl w:val="0"></w:rtl><w:lang w:val="en-US"></w:lang></w:rPr><w:t>this is one paragraph</w:t></w:r></w:p><w:p><w:pPr><w:pStyle w:val="Body A"></w:pStyle></w:pPr></w:p><w:p><w:pPr><w:pStyle w:val="Body A"></w:pStyle></w:pPr></w:p><w:p><w:pPr><w:pStyle w:val="Body A"></w:pStyle></w:pPr><w:r><w:rPr><w:rtl w:val="0"></w:rtl><w:lang w:val="en-US"></w:lang></w:rPr><w:t>this is another paragraph</w:t></w:r></w:p><w:p><w:pPr><w:pStyle w:val="Body A"></w:pStyle></w:pPr></w:p><w:p><w:pPr><w:pStyle w:val="Body A"></w:pStyle></w:pPr></w:p><w:p><w:pPr><w:pStyle w:val="Body A"></w:pStyle></w:pPr><w:r><w:rPr><w:rtl w:val="0"></w:rtl><w:lang w:val="en-US"></w:lang></w:rPr><w:t>and a third or fourth paragraph.</w:t></w:r></w:p><w:p><w:pPr><w:pStyle w:val="Body A"></w:pStyle></w:pPr></w:p><w:p><w:pPr><w:pStyle w:val="Body A"></w:pStyle></w:pPr><w:r><w:rPr><w:rtl w:val="0"></w:rtl><w:lang w:val="en-US"></w:lang></w:rPr><w:t xml:space="preserve">and some </w:t></w:r><w:r><w:rPr><w:b w:val="1"></w:b><w:bCs w:val="1"></w:bCs><w:rtl w:val="0"></w:rtl><w:lang w:val="en-US"></w:lang></w:rPr><w:t>bold</w:t></w:r><w:r><w:rPr><w:rtl w:val="0"></w:rtl><w:lang w:val="en-US"></w:lang></w:rPr><w:t xml:space="preserve"> and </w:t></w:r><w:r><w:rPr><w:i w:val="1"></w:i><w:iCs w:val="1"></w:iCs><w:rtl w:val="0"></w:rtl><w:lang w:val="en-US"></w:lang></w:rPr><w:t>italic</w:t></w:r><w:r><w:rPr><w:rtl w:val="0"></w:rtl><w:lang w:val="en-US"></w:lang></w:rPr><w:t xml:space="preserve"> and </w:t></w:r><w:r><w:rPr><w:rFonts w:ascii="Hack Regular" w:hAnsi="Hack Regular"></w:rFonts><w:sz w:val="16"></w:sz><w:szCs w:val="16"></w:szCs><w:rtl w:val="0"></w:rtl><w:lang w:val="en-US"></w:lang></w:rPr><w:t>different</w:t></w:r><w:r><w:rPr><w:rtl w:val="0"></w:rtl><w:lang w:val="en-US"></w:lang></w:rPr><w:t xml:space="preserve"> text</w:t></w:r></w:p><w:sectPr><w:headerReference w:type="default" r:id="rId4"></w:headerReference><w:footerReference w:type="default" r:id="rId5"></w:footerReference><w:pgSz w:w="12240" w:h="15840" w:orient="portrait"></w:pgSz><w:pgMar w:top="1440" w:right="1440" w:bottom="1440" w:left="1440" w:header="720" w:footer="864"></w:pgMar><w:bidi w:val="0"></w:bidi></w:sectPr></w:body></w:document>';
-	const p = new xml.XMLParser(opts);
-	const b = new xml.XMLBuilder(opts);
-
-	const xo = p.parse(xmlString);
-	const out = b.build(xo);
-
-	expect(xmlString).toEqual(out);
-});
-
 describe('Docx', () => {
 	test('exporter does not corrupt text', async () => {
 		let doc0 = await DocFile.createDocFile(await blobFromFile(docPath));
@@ -102,7 +84,7 @@ describe('Docx', () => {
 
 		const b0 = await bytesFromDocXML(docPath);
 		const b1 = await bytesFromDocXML(docPathOut);
-		// expect(b0.byteLength).toStrictEqual(b1.byteLength);
+		expect(b0.byteLength).toStrictEqual(b1.byteLength);
 		expect(b0).toStrictEqual(b1);
 	});
 
