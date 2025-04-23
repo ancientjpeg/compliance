ef25be00416099be21ead8de44913d501c13575d
Jackson Kaplan <JwyattK@gmail.com>
2025-04-22 20:24:59 -0700

    add state functions

--- src/lib/state/replacer.svelte.ts
+++ src/lib/state/replacer.svelte.ts
@@ -1,4 +1,36 @@
 import defaultReplacer from "./defaultReplacer";
+import { SvelteMap } from "svelte/reactivity";
 import type { Replacer } from "./replacer.d";
+import replacerVerify from "./replacerVerify";
 
-const replacer = $state(defaultReplacer);
+export let replacer = $state(structuredClone(defaultReplacer));
+
+export function createReplacerFromText(replacerText: string): Replacer {
+  let obj;
+
+  try {
+    obj = JSON.parse(replacerText);
+  } catch (e) {
+    throw new SyntaxError(`Failed to parse replacer JSON: ${replacerText}`);
+  }
+
+  const repl: Replacer = new Map<string, string>(obj);
+
+  const verification = replacerVerify(repl);
+  if (verification !== null) {
+    throw new SyntaxError(
+      `Invalid replacer; found keys in values: ${verification}`,
+    );
+  }
+
+  return repl;
+}
+
+export function attemptReplacerUpdate(replacerText: string): void {
+  replacer = createReplacerFromText(replacerText);
+}
+
+export function resetReplacer(): void {
+  replacer = new SvelteMap(defaultReplacer);
+  console.error("SVELTEMAP CLONE!");
+}
